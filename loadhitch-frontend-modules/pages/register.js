603f6fd39350dd75ef68f7dca3ef5c861156e764
BrightonDube <BrightonDube@users.noreply.github.com>
2021-02-14 20:04:54 +0200

    refactor form validation

--- loadhitch-frontend-modules/pages/register.js
+++ loadhitch-frontend-modules/pages/register.js
@@ -15,37 +15,33 @@ const about = () => {
           </Col>
         </Row>
         <Row className="justify-content-center w-100 py-5">
-          <Col>
-            <Form className="w-50 mx-auto" onSubmit={handleSubmit(onSubmit)}>
+          <Col xs={12} sm={10} md={8}>
+            <Form className="w-90 mx-auto" onSubmit={handleSubmit(onSubmit)}>
               <Form.Group>
                 <Form.Control
                   size="lg"
                   type="text"
                   placeholder="First Name"
-                  required="You must enter your name"
                   name="Name"
                   ref={register({ required: true, maxLength: 20 })}
                 />
-                {errors ? (
-                  <div className={styles.errorText}>
-                    <p>{errors.name}</p>
+                {errors.Name && (
+                  <div className={styles.errorsText}>
+                    First name is required
                   </div>
-                ) : null}
+                )}
               </Form.Group>
               <Form.Group>
                 <Form.Control
                   size="lg"
                   type="text"
                   placeholder="Last Name"
-                  required="You must enter your last name"
                   name="LName"
                   ref={register({ required: true, maxLength: 20 })}
                 />
-                {errors ? (
-                  <div className={styles.errorText}>
-                    <p>{errors.name}</p>
-                  </div>
-                ) : null}
+                {errors.LName && (
+                  <div className={styles.errorsText}>Last name is required</div>
+                )}
               </Form.Group>
 
               <Form.Group>
@@ -63,9 +59,11 @@ const about = () => {
                   type="email"
                   placeholder="Work Email"
                   name="email"
-                  required="Enter a valid work email"
-                  ref={register}
+                  ref={register({ required: true, pattern: /^\S+@\S+$/i })}
                 />
+                {errors.email && (
+                  <div className={styles.errorsText}>Email is required</div>
+                )}
               </Form.Group>
 
               <Form.Group>
@@ -74,9 +72,13 @@ const about = () => {
                   type="tel"
                   placeholder="Mobile Number"
                   name="mobileNumber"
-                  required="Enter a valid mobile number"
-                  ref={register}
+                  ref={register({ required: true, maxLength: 20 })}
                 />
+                {errors.mobileNumber && (
+                  <div className={styles.errorsText}>
+                    Your mobile number is required
+                  </div>
+                )}
               </Form.Group>
               <Form.Group>
                 <Form.Control
@@ -84,16 +86,18 @@ const about = () => {
                   defaultValue={null}
                   size="lg"
                   name="role"
-                  required="Make a selection"
-                  ref={register}
+                  ref={register({ required: true, maxLength: 20 })}
                 >
                   <option>Your role can be best described as</option>
-                  <option>Owner Operator</option>
-                  <option>Company Owner</option>
-                  <option>Dispatcher</option>
-                  <option>Operations Manager</option>
-                  <option>Other</option>
+                  <option value="owner operator">Owner Operator</option>
+                  <option value="company owner">Company Owner</option>
+                  <option value="dispatcher">Dispatcher</option>
+                  <option value="operations manager">Operations Manager</option>
+                  <option value="other">Other</option>
                 </Form.Control>
+                {errors.role && (
+                  <div className={styles.errorsText}>Role is required</div>
+                )}
               </Form.Group>
 
               <Form.Group>
@@ -102,14 +106,20 @@ const about = () => {
                   defaultValue={null}
                   size="lg"
                   name="operation"
-                  required="make a selection"
-                  ref={register}
+                  ref={register({ required: true })}
                 >
                   <option>My Carrier operations is best described as</option>
-                  <option>Within Canadian Border</option>
-                  <option>Across Canadian Border</option>
-                  <option>Within and across the Canadian border</option>
+                  <option value="within">Within Canadian Border</option>
+                  <option value="across">Across Canadian Border</option>
+                  <option value="within and across">
+                    Within and across the Canadian border
+                  </option>
                 </Form.Control>
+                {errors.operation && (
+                  <div className={styles.errorsText}>
+                    Make a valid Selection
+                  </div>
+                )}
               </Form.Group>
               <Form.Group>
                 <Form.Control
@@ -117,14 +127,18 @@ const about = () => {
                   defaultValue={null}
                   size="lg"
                   name="destination"
-                  required="make a selection"
-                  ref={register}
+                  ref={register({ required: true })}
                 >
                   <option>Please Specify Destination</option>
-                  <option>Mexico</option>
-                  <option>USA</option>
-                  <option>Mexico and USA</option>
+                  <option value="Mexico">Mexico</option>
+                  <option value="USA">USA</option>
+                  <option value="both">Mexico and USA</option>
                 </Form.Control>
+                {errors.destination && (
+                  <div className={styles.errorsText}>
+                    Make a valid selection
+                  </div>
+                )}
               </Form.Group>
               <Form.Group>
                 <Form.Control
@@ -132,9 +146,13 @@ const about = () => {
                   type="text"
                   placeholder="DOT Number"
                   name="dot"
-                  required="DOT Number is required"
-                  ref={register}
+                  ref={register({ required: true })}
                 />
+                {errors.dot && (
+                  <div className={styles.errorsText}>
+                    DOT Number is required
+                  </div>
+                )}
               </Form.Group>
               <Form.Group>
                 <Form.Control
@@ -142,13 +160,17 @@ const about = () => {
                   defaultValue={null}
                   size="lg"
                   name="area"
-                  required
-                  ref={register}
+                  ref={register({ required: true })}
                 >
                   <option>I travel within Ontario</option>
-                  <option>YES</option>
-                  <option>NO</option>
+                  <option value="yes">YES</option>
+                  <option value="no">NO</option>
                 </Form.Control>
+                {errors.area && (
+                  <div className={styles.errorsText}>
+                    Choose a valid location
+                  </div>
+                )}
               </Form.Group>
               <Form.Group>
                 <Form.Control
@@ -156,15 +178,19 @@ const about = () => {
                   defaultValue={null}
                   size="lg"
                   name="regLocation"
-                  required
-                  ref={register}
+                  ref={register({ required: true })}
                 >
                   <option>
                     My Truck/s is plated in Ontario, Mexico or USA?
                   </option>
-                  <option>YES</option>
-                  <option>NO</option>
+                  <option value="yes">YES</option>
+                  <option value="no">NO</option>
                 </Form.Control>
+                {errors.regLocation && (
+                  <div className={styles.errorsText}>
+                    Place of registration is required
+                  </div>
+                )}
               </Form.Group>
               <Form.Group>
                 <Form.Control
@@ -172,9 +198,13 @@ const about = () => {
                   type="text"
                   placeholder="CVOR Number"
                   name="cvor"
-                  required
-                  ref={register}
+                  ref={register({ required: true })}
                 />
+                {errors.cvor && (
+                  <div className={styles.errorsText}>
+                    CVOR number is required
+                  </div>
+                )}
                 <Form.Group></Form.Group>
               </Form.Group>
               <Form.Group controlId="formBasicPassword">
@@ -182,7 +212,6 @@ const about = () => {
                   type="password"
                   placeholder="Password"
                   size="lg"
-                  required="Enter a valid password"
                   name="password"
                   ref={register({
                     required: true,
@@ -190,16 +219,29 @@ const about = () => {
                     maxLength: 20,
                   })}
                 />
+                {errors.password && (
+                  <div className={styles.errorsText}>
+                    Password must be at least 8 characters long
+                  </div>
+                )}
               </Form.Group>
               <Form.Group controlId="formBasicPassword">
                 <Form.Control
                   type="password"
                   placeholder="Password"
                   size="lg"
-                  required="required"
                   name="password2"
-                  ref={register}
+                  ref={register({
+                    required: true,
+                    minLength: 8,
+                    maxLength: 20,
+                  })}
                 />
+                {errors.password2 && (
+                  <div className={styles.errorsText}>
+                    Valid password is required
+                  </div>
+                )}
               </Form.Group>
               <Form.Group>
                 <Form.Control
@@ -207,28 +249,36 @@ const about = () => {
                   defaultValue="email"
                   size="lg"
                   name="medium"
-                  required
-                  ref={register}
+                  ref={register({ required: true, minLength: 6 })}
                 >
                   <option>
                     Where can we send your login Security Code too?*
                   </option>
-                  <option>Mobile Number</option>
-                  <option>Work Email Address</option>
+                  <option value="Mobile Number">Mobile Number</option>
+                  <option value="email">Work Email Address</option>
                 </Form.Control>
+                {errors.medium && (
+                  <div className={styles.errorsText}>
+                    Make a valid selection
+                  </div>
+                )}
               </Form.Group>
               <Form.Group as={Row} controlId="formHorizontalCheck">
                 <Col sm={{ span: 10 }}>
                   <Form.Check
                     label="Agree to Terms and Conditions"
                     name="terms"
-                    required
-                    ref={register}
+                    ref={register({ required: true })}
                   />
+                  {errors.name && (
+                    <div className={styles.errorsText}>
+                      You must agree to the terms and conditions
+                    </div>
+                  )}
                 </Col>
               </Form.Group>
               <Button
-                className={styles.submitButton}
+                className={`mx-auto ${styles.submitButton}`}
                 variant="success"
                 type="submit"
                 size="lg"
