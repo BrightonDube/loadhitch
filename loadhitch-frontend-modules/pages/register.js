ec8cad27d488e00fe80999f8078d49627783ff8a
BrightonDube <BrightonDube@users.noreply.github.com>
2021-02-14 15:27:46 +0200

    style registration form

--- loadhitch-frontend-modules/pages/register.js
+++ loadhitch-frontend-modules/pages/register.js
@@ -7,33 +7,164 @@ const about = () => {
     <div className={styles.formStyle}>
       <Container>
         <Row>
-          <Col className="text-center">
+          <Col className="text-center pt-5">
             <h1>Register</h1>
           </Col>
         </Row>
-        <Row className="justify-content-center w-100">
+        <Row className="justify-content-center w-100 py-5">
           <Col>
             <Form className="w-50 mx-auto">
-              <Form.Group controlId="formBasicName">
-                <Form.Label>First Name</Form.Label>
+              <Form.Group>
                 <Form.Control
                   size="lg"
                   type="text"
-                  placeholder="Enter your first name"
+                  placeholder="First Name"
+                  required
                 />
               </Form.Group>
-              <Form.Group controlId="formBasicLastName">
-                <Form.Label>Last Name</Form.Label>
-                <Form.Control type="text" placeholder="Enter your last name" />
+              <Form.Group>
+                <Form.Control
+                  size="lg"
+                  type="text"
+                  placeholder="Last Name"
+                  required
+                />
+              </Form.Group>
+
+              <Form.Group>
+                <Form.Control
+                  size="lg"
+                  type="text"
+                  placeholder="Company Name"
+                  required
+                />
+              </Form.Group>
+              <Form.Group>
+                <Form.Control
+                  size="lg"
+                  type="email"
+                  placeholder="Work Email"
+                  required
+                />
+              </Form.Group>
+
+              <Form.Group>
+                <Form.Control
+                  size="lg"
+                  type="tel"
+                  placeholder="Mobile Number"
+                  required
+                />
+              </Form.Group>
+              <Form.Group>
+                <Form.Control
+                  as="select"
+                  defaultValue="Your role can be best described as"
+                  size="lg"
+                  required
+                >
+                  <option>Your role can be best described as</option>
+                  <option>Owner Operator</option>
+                  <option>Company Owner</option>
+                  <option>Dispatcher</option>
+                  <option>Operations Manager</option>
+                  <option>Other</option>
+                </Form.Control>
+              </Form.Group>
+
+              <Form.Group>
+                <Form.Control
+                  as="select"
+                  defaultValue="My Carrier operations is best described as"
+                  size="lg"
+                  required
+                >
+                  <option>My Carrier operations is best described as</option>
+                  <option>Within Canadian Border</option>
+                  <option>Across Canadian Border</option>
+                  <option>Within and across the Canadian border</option>
+                </Form.Control>
+              </Form.Group>
+              <Form.Group>
+                <Form.Control
+                  as="select"
+                  defaultValue="Please Specify Destination"
+                  size="lg"
+                  required
+                >
+                  <option>Please Specify Destination</option>
+                  <option>Mexico</option>
+                  <option>USA</option>
+                  <option>Mexico and USA</option>
+                </Form.Control>
+              </Form.Group>
+              <Form.Group>
+                <Form.Control
+                  size="lg"
+                  type="text"
+                  placeholder="DOT Number"
+                  required
+                />
+              </Form.Group>
+              <Form.Group>
+                <Form.Control as="select" defaultValue="Yes" size="lg" required>
+                  <option>I travel within Ontario</option>
+                  <option>YES</option>
+                  <option>NO</option>
+                </Form.Control>
+              </Form.Group>
+              <Form.Group>
+                <Form.Control as="select" defaultValue="Yes" size="lg" required>
+                  <option>
+                    My Truck/s is plated in Ontario, Mexico or USA?
+                  </option>
+                  <option>YES</option>
+                  <option>NO</option>
+                </Form.Control>
+              </Form.Group>
+              <Form.Group>
+                <Form.Control
+                  size="lg"
+                  type="text"
+                  placeholder="CVOR Number"
+                  required
+                />
+                <Form.Group></Form.Group>
               </Form.Group>
               <Form.Group controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
+                <Form.Control
+                  type="password"
+                  placeholder="Password"
+                  size="lg"
+                />
               </Form.Group>
-              <Form.Group controlId="formBasicCheckbox">
-                <Form.Check type="checkbox" label="Check me out" />
+              <Form.Group controlId="formBasicPassword">
+                <Form.Control
+                  type="password"
+                  placeholder="Password"
+                  size="lg"
+                />
               </Form.Group>
-              <Button variant="success" type="submit">
+              <Form.Group>
+                <Form.Control
+                  as="select"
+                  defaultValue="email"
+                  size="lg"
+                  required
+                >
+                  <option>
+                    Where can we send your login Security Code too?*
+                  </option>
+                  <option>Mobile Number</option>
+                  <option>Work Email Address</option>
+                </Form.Control>
+              </Form.Group>
+              <Button
+                className={styles.submitButton}
+                variant="success"
+                type="submit"
+                size="lg"
+              >
                 Submit
               </Button>
             </Form>
